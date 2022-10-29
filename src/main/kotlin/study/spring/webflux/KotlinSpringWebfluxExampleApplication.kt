a5981a90b7b9d22dbf9f63e7fa782b7f61211481
홍준성 <ghdwnstjd13@gmail.com>
2022-10-29 10:12:29 +0900

    New Project > git init

---
+++ src/main/kotlin/study/spring/webflux/KotlinSpringWebfluxExampleApplication.kt
@@ -0,0 +1,11 @@
+package study.spring.webflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringWebfluxExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringWebfluxExampleApplication>(*args)
+}
